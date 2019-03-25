4f4fe6317fbc75a99362252ecf8ff035f1e8f125
Sergey Krasnoyarov <krasnoyarovsergei@gmail.com>
2019-03-25 22:15:27 +0500

    add second slider and color scheme

--- js/main.js
+++ js/main.js
@@ -20,3 +20,26 @@ $('.direction').slick({
 
     ]
 });
+
+$('.direction_2').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+
+    ]
+});
