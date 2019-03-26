4bb3a8c54e1875560a22856db52f385635722d2d
Sergey Krasnoyarov <krasnoyarovsergei@gmail.com>
2019-03-26 08:02:59 +0500

    adding post photos, personal form, comments, charters

--- js/main.js
+++ js/main.js
@@ -43,3 +43,26 @@ $('.direction_2').slick({
 
     ]
 });
+
+$('.comments_blocks').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+
+    ]
+});
